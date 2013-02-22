6c354bb26c9f0538231a883c5499abb2cec57351
Ryan Farley <ryan@farleyzone.com>
2013-02-21 23:43:12 -0700

    Minor refactoring and cleanup

--- SalesLogixUserLogins/Managers/EncryptionManager.cs
+++ SalesLogixUserLogins/Managers/EncryptionManager.cs
@@ -5,36 +5,38 @@ namespace FX.SalesLogix.Utility.UserLogins.Managers
 {
 	public class EncryptionManager
 	{
-		public static string Decrypt(string srcValue)
+		public static string Decrypt(string sourceValue)
 		{
-			return Decrypt(srcValue, null);
+			return Decrypt(sourceValue, null);
 		}
 
-		public static string Decrypt(string srcValue, string key)
+		public static string Decrypt(string sourceValue, string key)
 		{
-			string str = string.Empty;
-			srcValue = srcValue.Trim();
-			key = key.Trim();
+			string newValue = string.Empty;
+			sourceValue = (sourceValue ?? string.Empty).Trim();
+			key = (key ?? string.Empty).Trim();
 
-			if ((key == null) || (key.Length == 0))
-			{
+			if (string.IsNullOrEmpty(key))
 				key = "SLSLGX";
-			}
+			
 			int length = key.Length;
 			int startIndex = 0;
-			if ((srcValue != null) && (srcValue.Length > 2))
+
+			if (sourceValue.Length > 2)
 			{
 				int num3;
-				int.TryParse(srcValue.Substring(0, 2), NumberStyles.HexNumber, null, out num3);
-				int num4 = srcValue.Length;
+				int.TryParse(sourceValue.Substring(0, 2), NumberStyles.HexNumber, null, out num3);
+				int num4 = sourceValue.Length;
 				int num5 = 2;
+
 				while (num5 < num4)
 				{
 					int num6;
-					int.TryParse(srcValue.Substring(num5, 2), NumberStyles.HexNumber, null, out num6);
+					int.TryParse(sourceValue.Substring(num5, 2), NumberStyles.HexNumber, null, out num6);
 					num5 += 2;
 					int num7 = num6 ^ key.Substring(startIndex, 1).ToCharArray()[0];
 					startIndex++;
+
 					if (startIndex >= length)
 					{
 						startIndex = 0;
@@ -47,50 +49,54 @@ namespace FX.SalesLogix.Utility.UserLogins.Managers
 					{
 						num7 -= num3;
 					}
-					str = str + ((char) num7);
+
+					newValue = newValue + ((char) num7);
 					num3 = num6;
 				}
 			}
-			return str.Trim();
+			return newValue.Trim();
 		}
 
-		public static string Encrypt(string srcValue)
+		public static string Encrypt(string sourceValue)
 		{
-			return Encrypt(srcValue, null);
+			return Encrypt(sourceValue, null);
 		}
 
-		public static string Encrypt(string srcValue, string key)
+		public static string Encrypt(string sourceValue, string key)
 		{
-			string str = string.Empty;
-			srcValue = srcValue.Trim();
+			string newValue = string.Empty;
+			sourceValue = sourceValue.Trim();
 			key = key.Trim();
 
-			if ((srcValue != null) && (srcValue.Length > 0))
+			if (sourceValue.Length > 0)
 			{
-				if ((key == null) || (key.Length == 0))
-				{
+				if (string.IsNullOrEmpty(key))
 					key = "SLSLGX";
-				}
+				
 				int length = key.Length;
 				int num2 = 0;
 				int maxValue = 0x100;
 				int num4 = new Random().Next(maxValue);
-				str = string.Format("{0:X2}", num4);
-				int num5 = srcValue.Length;
+
+				newValue = string.Format("{0:X2}", num4);
+				int num5 = sourceValue.Length;
+
 				for (int i = 0; i < num5; i++)
 				{
-					int num7 = (((byte) srcValue[i]) + num4) % 0xff;
+					int num7 = (((byte) sourceValue[i]) + num4) % 0xff;
 					num7 ^= (byte) key[num2];
 					num2++;
+
 					if (num2 == length)
 					{
 						num2 = 0;
 					}
-					str = str + string.Format("{0:X2}", num7);
+
+					newValue = newValue + string.Format("{0:X2}", num7);
 					num4 = num7;
 				}
 			}
-			return str.Trim();
+			return newValue.Trim();
 		}
 	}
 }
